9bb2eeef11c57fdd1655df894bd78567861d039e
Ruggero <ruggerorebellato@gmail.com>
2017-10-17 16:18:44 -0200

    Add check-coverage(nyc-istambul) to prepush and test to precommit

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,11 @@
 const FizzBuzz = (num) => {
+  if (num === 0) return num;
   if (num % 5 === 0 && num % 3 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
   return num;
 };
+
+const foo = () => 'fuck man';
 
 export default FizzBuzz;
